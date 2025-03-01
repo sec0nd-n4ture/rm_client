03ca4d40897e67d3decf1a0c5fc4df0a9b9d3c8c
sec0nd-n4ture <a644cr@gmail.com>
2025-03-01 21:02:31 +0300

    add soldat executable directory to config

--- mod_launcher.py
+++ mod_launcher.py
@@ -1,8 +1,8 @@
-from mod_config import SOLDAT_SERVER_ADDRESS
+from mod_config import SOLDAT_SERVER_ADDRESS, SOLDAT_EXECUTABLE_DIR
 from subprocess import Popen
 import os
 import time
 
-Popen(f"C:\Soldat\soldat.exe -joinurl soldat://{SOLDAT_SERVER_ADDRESS[0]}:{SOLDAT_SERVER_ADDRESS[1]}")
+Popen(f"{SOLDAT_EXECUTABLE_DIR} -joinurl soldat://{SOLDAT_SERVER_ADDRESS[0]}:{SOLDAT_SERVER_ADDRESS[1]}")
 time.sleep(3)
 os.system("python mod_main.py")
