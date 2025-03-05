ca0b56a11c0dd251f17d01a1cee7ad8757fdf9a0
sec0nd-n4ture <a644cr@gmail.com>
2025-03-05 22:05:10 +0300

    add proper server

--- mod_launcher.py
+++ mod_launcher.py
@@ -1,8 +1,8 @@
-from mod_config import SOLDAT_SERVER_ADDRESS, SOLDAT_EXECUTABLE_DIR
+from mod_config import SOLDAT_SERVER_ADDRESS, SOLDAT_EXECUTABLE_DIR, SOLDAT_SERVER_PASSWORD
 from subprocess import Popen
 import os
 import time
 
-Popen(f"{SOLDAT_EXECUTABLE_DIR} -joinurl soldat://{SOLDAT_SERVER_ADDRESS[0]}:{SOLDAT_SERVER_ADDRESS[1]}")
+Popen(f"{SOLDAT_EXECUTABLE_DIR} -joinurl soldat://{SOLDAT_SERVER_ADDRESS[0]}:{SOLDAT_SERVER_ADDRESS[1]}/{SOLDAT_SERVER_PASSWORD}")
 time.sleep(3)
 os.system("python mod_main.py")
